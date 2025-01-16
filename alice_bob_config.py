583cf3216d9a940fb082e46f0fc4373fec884376
Raphaël AZOU <raphael.azou@ensta-bretagne.org>
2025-01-16 20:48:46 +0100

    [code] - Alice & Bob basic with embedded dsl

---
+++ alice_bob_config.py
@@ -0,0 +1,48 @@
+from piece import Piece
+from soup import Soup
+
+
+class AliceBobConfig:
+
+    def __init__(self, state_alice = 'i', state_bob = 'i'):
+        self.state_alice = state_alice
+        self.state_bob = state_bob
+
+    def __repr__(self):
+        return "Alice: " + str(self.state_alice) + ", Bob: " + str(self.state_bob)
+    
+    def __hash__(self):
+        return hash((self.state_alice, self.state_bob))
+    
+    def __eq__(self, comparative):
+        """
+            Compares an object to the current one.
+            @comparative: the object to compare to the self one
+            @return True if both objects are equal, else false
+        """
+        if (not isinstance(comparative, AliceBobConfig)):
+            return False;
+        return (self.state_alice == comparative.state_alice) and (self.state_bob == comparative.state_bob)
+    
+
+def alice_and_bob():
+     
+    def alice_state_c(config : AliceBobConfig):
+        config.state_alice = 'c'
+
+    def alice_state_i(config : AliceBobConfig):
+        config.state_alice = 'i'
+
+    def bob_state_c(config : AliceBobConfig):
+        config.state_bob = 'c'
+
+    def bob_state_i(config : AliceBobConfig):
+        config.state_bob = 'i'
+
+    p1 = Piece("Alice c", lambda config : config.state_alice == 'i', alice_state_c)
+    p2 = Piece("Alice i", lambda config : config.state_alice == 'c', alice_state_i)
+    
+    p3 = Piece("Bob c", lambda config : config.state_bob == 'i', bob_state_c)
+    p4 = Piece("Bob i", lambda config : config.state_bob == 'c', bob_state_i)
+
+    return Soup(AliceBobConfig(), [p1, p2, p3, p4])
--- soup_semantic.py
+++ soup_semantic.py
@@ -5,6 +5,7 @@ from program_1_config import program1
 from rr2rg import RR2RG
 from predicate_finder import predicate_finder
 from parent_tracer import ParentTracer
+from alice_bob_config import alice_and_bob, AliceBobConfig
 
 class SoupSemantic(RootedRelation):
 
@@ -47,6 +48,44 @@ def main():
             print(state)
     else:
         print("Aucun deadlock trouvé.")
+
+    print("------- Alice&Bob   -------")
+
+    program = alice_and_bob()
+    soup_semantic = SoupSemantic(program)
+
+    graph = ParentTracer(RR2RG(soup_semantic))
+
+    print("[+] Recherche d'un deadlock...")
+
+    is_deadlock = predicate_finder(graph, lambda state: len(soup_semantic.actions(state)) == 0)
+    
+    if is_deadlock:
+        print("[+] Deadlock trouvée !")
+
+
+        print("[+] Trace du deadlock:")
+        trace = graph.getTrace(is_deadlock)
+        for state in trace:
+            print(state)
+    else:
+        print("Aucun deadlock trouvé.")
+
+    print("[+] Recherche du cas (c,c)...")
+
+    forbiden_state = predicate_finder(graph, lambda state: state == AliceBobConfig('c', 'c'))
+    
+    if forbiden_state:
+        print("[+] état interdit trouvée !")
+
+
+        print("[+] Trace :")
+        trace = graph.getTrace(forbiden_state)
+        for state in trace:
+            print(state)
+    else:
+        print("Aucun deadlock trouvé.")
+
 
 
 if __name__ == '__main__':
